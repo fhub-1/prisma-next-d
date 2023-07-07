72806a4f225116637c9bde83e7a0807ae918883d
fhub-1 <flutterhub1.21.0@gmail.com>
2023-07-07 13:46:53 +0300

    set

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,5 +1,7 @@
+import prismadb from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { SettingsForm } from "./components/setting-form";
 
 interface SettingPageProps {
   params: {
@@ -7,12 +9,27 @@ interface SettingPageProps {
   };
 }
 
-const SettingPage: React.FC<SettingPageProps> = ({ params }) => {
+const SettingPage: React.FC<SettingPageProps> = async ({ params }) => {
   const { userId } = auth();
   if (!userId) {
     redirect("/sign-in");
   }
-  return <div>settiiiing</div>;
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+  if (!store) {
+    redirect("/");
+  }
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  );
 };
 
 export default SettingPage;
